0e9f56b1646bd151e87c8cf3271a91d5d0b2f30f
gami-official <k_fuchigami@gamigami.email>
2026-04-18 17:06:05 +0900

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import { Header } from "@/components/header"
 import { Hero } from "@/components/hero"
+import { About } from "@/components/about"
 
 export default function Home() {
   return (
     <main>
       <Header />
       <Hero />
+      <About />
     </main>
   )
 }
